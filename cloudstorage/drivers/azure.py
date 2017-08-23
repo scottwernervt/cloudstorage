09063278f078c5c84276836cb4a784702a33a745
Scott Werner <scott.werner.vt@gmail.com>
2017-08-23 19:28:54 -0400

    Add enable / disable for cdn and get blobs for test cleanup.

--- cloudstorage/drivers/azure.py
+++ cloudstorage/drivers/azure.py
@@ -9,24 +9,31 @@ except ImportError:
     from httpstatus import HTTPStatus
 from typing import Dict, Iterable, List, Union
 
-from azure.common import AzureMissingResourceHttpError, AzureHttpError, AzureConflictHttpError
+from azure.common import AzureMissingResourceHttpError
+from azure.common import AzureHttpError
+from azure.common import AzureConflictHttpError
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob.models import Blob as AzureBlob
 from azure.storage.blob.models import Container as AzureContainer
+from azure.storage.blob.models import Include
 
 from inflection import underscore
 
-from cloudstorage.exceptions import (
-    NotFoundError, CloudStorageError,
-)
-from cloudstorage.base import (
-    Blob, Container, ContentLength, Driver,
-    ExtraOptions, FileLike, FormPost, MetaData,
-)
-from cloudstorage.messages import (
-    CONTAINER_NOT_FOUND, CONTAINER_EXISTS,
-)
+from cloudstorage.exceptions import NotFoundError
+from cloudstorage.exceptions import CloudStorageError
+from cloudstorage.base import Blob
+from cloudstorage.base import Container
+from cloudstorage.base import ContentLength
+from cloudstorage.base import Driver
+from cloudstorage.base import ExtraOptions
+from cloudstorage.base import FileLike
+from cloudstorage.base import FormPost
+from cloudstorage.base import MetaData
+
+from cloudstorage.messages import CONTAINER_NOT_FOUND
+from cloudstorage.messages import CONTAINER_EXISTS
+from cloudstorage.messages import BLOB_NOT_FOUND
 
 logger = logging.getLogger(__name__)
 
@@ -74,7 +81,14 @@ class AzureStorageDriver(Driver):
         return normalized
 
     def _get_azure_blob(self, container_name: str, blob_name: str) -> AzureBlob:
-        pass
+        try:
+            azure_blob = self.service.get_blob_properties(container_name,
+                                                          blob_name)
+        except AzureMissingResourceHttpError as err:
+            logger.debug(err)
+            raise NotFoundError(BLOB_NOT_FOUND % (blob_name, container_name))
+
+        return azure_blob
 
     def _wrap_azure_blob(self, container: Container,
                          azure_blob: AzureBlob) -> Blob:
@@ -155,17 +169,23 @@ class AzureStorageDriver(Driver):
         raise NotImplementedError
 
     def delete_container(self, container: Container) -> None:
-        azure_container = self._get_azure_container(container.name)
+        self._get_azure_container(container.name)
+        # TODO: Throw NotEmpty error if blobs exist
         self.service.delete_container(container.name, fail_not_exist=False)
 
     def container_cdn_url(self, container: Container) -> str:
         pass
 
     def enable_container_cdn(self, container: Container) -> bool:
-        pass
+        self._get_azure_container(container.name)
+        self.service.set_container_acl(container.name,
+                                       public_access=PublicAccess.Container)
+        return True
 
     def disable_container_cdn(self, container: Container) -> bool:
-        pass
+        self._get_azure_container(container.name)
+        self.service.set_container_acl(container.name, public_access=None)
+        return True
 
     def upload_blob(self, container: Container, filename: Union[str, FileLike],
                     blob_name: str = None, acl: str = None,
@@ -175,10 +195,15 @@ class AzureStorageDriver(Driver):
         pass
 
     def get_blob(self, container: Container, blob_name: str) -> Blob:
-        pass
+        self._get_azure_container(container.name)
 
     def get_blobs(self, container: Container) -> Iterable[Blob]:
-        pass
+        self._get_azure_container(container.name)
+
+        azure_blobs = self.service.list_blobs(container.name,
+                                              include=Include(metadata=True))
+        for azure_blob in azure_blobs:
+            yield self._wrap_azure_blob(container, azure_blob)
 
     def download_blob(self, blob: Blob,
                       destination: Union[str, FileLike]) -> None:
