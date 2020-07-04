b301b57c9f3320f70f9a0790d94df370eea9a334
Scott Werner <scott.werner.vt@gmail.com>
2020-07-04 11:45:03 -0400

    Remove pycharm inspections and use context manager for temp file fixture.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,42 +1,35 @@
 import os
 
-from tempfile import mkstemp
+from tempfile import NamedTemporaryFile
+from pathlib import Path
 
 import pytest
 
 from tests import settings
 from tests.helpers import random_container_name
 
-ROOT = os.path.dirname(os.path.realpath(__file__))
+PWD_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
+DATA_PATH = PWD_PATH.parents[0] / "data"
 
 
-@pytest.fixture(scope="module")
-def storage():
-    pass
-
-
-# noinspection PyShadowingNames
 @pytest.fixture(scope="module")
 def container(storage):
     container_name = random_container_name()
     container = storage.create_container(container_name)
-
     yield container
 
 
 @pytest.fixture(scope="session")
 def text_filename():
-    return os.path.join(ROOT, "data", settings.TEXT_FILENAME)
+    return DATA_PATH / settings.TEXT_FILENAME
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope="function")
 def text_stream(text_filename):
     with open(text_filename, "rb") as text_stream:
         yield text_stream
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope="function")
 def text_blob(container, text_filename):
     text_blob = container.upload_blob(text_filename)
@@ -47,20 +40,17 @@ def text_blob(container, text_filename):
         text_blob.delete()
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope="session")
 def binary_filename():
-    return os.path.join(ROOT, "data", settings.BINARY_FILENAME)
+    return DATA_PATH / Path(settings.BINARY_FILENAME)
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope="function")
 def binary_stream(binary_filename):
     with open(binary_filename, "rb") as binary_stream:
         yield binary_stream
 
 
-# noinspection PyShadowingNames
 @pytest.fixture(scope="function")
 def binary_blob(container, binary_filename):
     binary_blob = container.upload_blob(binary_filename)
@@ -73,6 +63,5 @@ def binary_blob(container, binary_filename):
 
 @pytest.fixture(scope="function")
 def temp_file():
-    _, path = mkstemp(prefix=settings.CONTAINER_PREFIX)
-    yield path
-    os.remove(path)
+    with NamedTemporaryFile(prefix=settings.CONTAINER_PREFIX) as temp_file:
+        yield temp_file.name
